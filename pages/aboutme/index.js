ef660dfa4af21c779792f69b76897533bab89289
Archit Choudhary <archit.digisocial@gmail.com>
2022-04-09 19:54:20 +0530

    removed image import statement

--- pages/aboutme/index.js
+++ pages/aboutme/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../../styles/Aboutme.module.css'
 
 export default function Aboutme() {
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Landing from '../components/Landing/landing'
-import Navigation from '../components/Navigation'
 
 export default function LandingPage() {
   return (
