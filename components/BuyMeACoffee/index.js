9baa3600f38207d82810afb60645331be69cc447
Archit Choudhary <archit.digisocial@gmail.com>
2022-04-09 16:09:27 +0530

    added rel="noreferrer"

--- components/BuyMeACoffee/index.js
+++ components/BuyMeACoffee/index.js
@@ -4,7 +4,7 @@ function BuyMeACoffee(props) {
 
   return( 
       <span className={styles.coffeeSpan}>
-          <a href="https://www.buymeacoffee.com/architdigii" target="_blank">
+          <a href="https://www.buymeacoffee.com/architdigii" target="_blank" rel="noreferrer">
               <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" />
         </a>
       </span>
